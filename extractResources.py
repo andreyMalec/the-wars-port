6f8cde5809b6aa8ff5bb48a9794b803051c15960
andreyMalec <andrey193728@yandex.ru>
2024-10-31 22:50:59 +0300

    sounds

--- extractResources.py
+++ extractResources.py
@@ -2,6 +2,7 @@ import zipfile
 from PIL import Image
 import os
 from pathlib import Path 
+import shutil
 
 def creep_resize(img, size=80, offset=0):
     # img = Image.open(srcimg)
@@ -16,14 +17,23 @@ offset = 0
 creep_size=80
 last = ""
 
+def makedirs(row1):
+    path = f"{'/'.join(row1.split('/')[:-1])}"
+    if not os.path.exists(path):
+        os.makedirs(path)
+
 with open('dict.csv', 'r', encoding='utf-8-sig') as file:
     for line in file:
         row = line.strip().split(';')
+
+        makedirs(row[1])
+
+        if (".wav" in row[0] or ".mp3" in row[0]):
+            shutil.copy(Path(f'tmp/res/raw/{row[0]}'), Path(row[1]))
+            break
+
         img = Image.open(f'tmp/res/drawable/{row[0]}')
 
-        path = f"{'/'.join(row[1].split('/')[:-1])}"
-        if not os.path.exists(path):
-            os.makedirs(path)
 
         res = ""
 
@@ -42,5 +52,4 @@ with open('dict.csv', 'r', encoding='utf-8-sig') as file:
         else:
             res = img
         res.save(f"{row[1]}")
-
 
